c98c87a23d761a920fa9087c93f36475c2164b95
roboterz <40050902+roboterz@users.noreply.github.com>
2026-08-13 03:14:30 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "1.110"
+        versionName = "1.1.001"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
